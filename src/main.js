2668aca5461806e8a5e3ac42bcc156201ebc75c6
Alex Schulz <schubz94@gmail.com>
2023-11-16 17:50:17 +0100

    nice try... nothing new

--- src/main.js
+++ src/main.js
@@ -2,14 +2,20 @@ import './style.css'
 import axios from 'https://esm.sh/axios'
 
 const quotes =  await axios.get('https://api.quotable.io/quotes/random?minLength=300&maxLength=350');
-const checkText = document.querySelector('.check-text');
-const counter = document.querySelector('.counter');
+const checkText = document.querySelector('#check-text');
+const counter = document.querySelector('#counter');
+
 
 let count = 0;
 
 let characterCount = quotes.data[0].content.length;
 
 counter.innerText = count +"/"+ characterCount;
-checkText.innerText = quotes.data[0].content;
 
+checkText.setAttribute("data-placeholder",  quotes.data[0].content);
+checkText.setAttribute("placeholder",  quotes.data[0].content);
+
+// checkText.innerText = quotes.data[0].content;
+checkText.style.height = checkText.scrollHeight + "px";
+checkText.classList.add("text-opacity-10");
 
